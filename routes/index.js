ee8c42bab2660352da3d38f8a5a80ce3878201a9
adisri06 <adi.sri06@gmail.com>
2024-06-22 12:14:20 +0530

    added some routes

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,38 @@ router.get('/users', async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
+router.get('/get_user_id/:username', async (req, res) => {
+  try {
+    console.log('hello', req.params)
+    const { username } = req.params;
+    const result = await pool.query('SELECT id FROM "users" WHERE username = $1', [username]);
+    if (result.rows.length === 0) {
+      return res.status(404).json({ msg: 'User not found' });
+    }
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+router.post('/addusers', async (req, res) => {
+  const { username, email, surname, age, height, weight } = req.body;
+  console.log('details are', req.body)
+  const query = `
+    INSERT INTO "users" (username, email, surname, age, height, weight)
+    VALUES ($1, $2, $3, $4, $5, $6)
+    RETURNING *;
+  `;
+
+  try {
+    const result = await pool.query(query, [username, email, surname, age, height, weight]);
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 router.get('/', (req, res) => {
   res.send('Hello, World!');
